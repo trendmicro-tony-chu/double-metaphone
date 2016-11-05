eb31b8a3658a9c8f488c75f9e43622e9e77b56b0
Titus Wormer <tituswormer@gmail.com>
2016-11-05 14:48:35 +0100

    Rewrite

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
 'use strict';
 
-/**
- * Dependencies.
- */
+/* Dependencies. */
+var doubleMetaphone = require('double-metaphone');
 
-var doubleMetaphone = require('wooorm/double-metaphone@0.1.4');
-
-/**
- * DOM elements.
- */
-
+/* Nodes. */
 var $input = document.getElementsByTagName('input')[0];
 var $output = document.getElementsByTagName('output')[0];
 
-/**
- * Event handlers.
- */
-
-function oninputchange() {
-    var value = doubleMetaphone($input.value);
-
-    if (value[0].length === 0 && value[1].length === 0) {
-        $output.textContent = '';
-
-        return;
-    }
-
-    $output.textContent = '["' + value[0] + '", "' + value[1] + '"]';
-}
-
-/**
- * Listen.
- */
-
+/* Listen. */
 $input.addEventListener('input', oninputchange);
 
-/**
- * Initial answer.
- */
-
+/* Initial answer. */
 oninputchange();
+
+/* Calculate. */
+function oninputchange() {
+  $output.textContent = JSON.stringify(doubleMetaphone($input.value))
+}
